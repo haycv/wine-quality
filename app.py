00e4783db5aaf60b34c5dbfe9a72ccd90c4f4fe0
haycv <155510965+haycv@users.noreply.github.com>
2024-01-14 18:54:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def predict():
             return render_template('index.html', error_message=str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host = '127.0.0.1', port=5000)
